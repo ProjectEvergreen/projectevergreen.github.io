aef43959143b4a633c7d9d976ceb1b9b80e7240d
Owen Buckley <owen@thegreenhouse.io>
2018-05-30 19:13:20 -0400

    misc styles and slack link

--- src/components/social-links-bar/social-links-bar.js
+++ src/components/social-links-bar/social-links-bar.js
@@ -14,6 +14,10 @@ class SocialLinksBar extends LitElement {
         <ul>
           <li class="github"><a href="https://github.com/ProjectEvergreen" class="github" target="_blank" rel="noopener"></a></li>
           <li class="twitter"><a href="https://twitter.com/PrjEvergreen" target="_blank" rel="noopener"></a></li>
+          <li class="slack">
+            <a href="https://join.slack.com/t/thegreenhouseio/shared_invite/enQtMzcyMzE2Mjk1MjgwLTFkNzY2NDQwOTg0MjRiYmY1ZDYyOGE1YzM0ZmQwNTgxZWI0NWU0YmYxZDhkNDQ3Y2Q3ODhjZTdiMTEwNzY5MTk" 
+              target="_blank" rel="noopener"></a>
+          </li>
         </ul>
 
       </div>
