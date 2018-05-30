9452925882c3066c6b33dfb6e5ffcc027228b1a9
Owen Buckley <owen@thegreenhouse.io>
2018-05-30 12:53:14 -0400

    looking good now I think

--- src/services/content-service.js
+++ src/services/content-service.js
@@ -4,27 +4,29 @@ const content = {
   home: [{
     title: 'Reduce',
     template: html`
-      <p>By leveraging the web platform itself, code can be more consistent, portable, and maintainable.  Reducing external dependencies
+      <p>By leveraging more of the the web's native capabilities, application's can become more consistent, portable, and maintainable.  Reducing external dependencies
       and being more "vanilla" can also provide passive improvements to performance and security.</p>
       <br/>
-      <p>Our philosophy to reduce overhead can be embodied by the mindset of: <i>Polyfills over frameworks, but don't re-invent the wheel if you don't need to.</i></p>
+      <p><i>Favor polyfills over frameworks, but don't re-invent the wheel if you don't need to.</i></p>
     `
   }, {
     title: 'Reuse',
     template: html`
-      <p>Component based development has become a widely adopted and pragmatic approach to modern UI web application development.</p>
+      <p>Component based development has become a widely adopted and pragmatic approach for modern web application development.</p>
       <br/>
-      <p>Reuse as a principal allows to write our own "Web" components that look a lot like components you may already be used to seeing and writing.</p>
-  
-      <!-- Code snippet -->
+      <p>Reuse as a principal allows for native "Web" components that end up looking a lot like components you may already be used to seeing and writing.</p>
+      <br/>
+      <a href="https://jsfiddle.net/thegreenhouseio/qjj9hvv9/1/" target="_blank" rel="noopener">
+        <img class="screenshot" src="https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/simple-component-example.png"/>
+      </a>
     `
   }, {
     title: 'Recycle',
     template: html`<p>It should be clear, the JavaScript community and ecosystem is amazing and inspiring! There are so many wonderful tools and options out there, and that's what
       makes it so great. You pick and choose what you need when you need it.  In fact, this project wouldn't be possible without the amazing work of tools like
-      <b>Babel</b>, <b>postCSS</b>, and <b>webpack</b>.</p>
+      <b>Babel</b>, <b>postCSS</b>, and <b>webpack</b>. ❤️</p>
       <br/>
-      <p>We believe in not sacrificing developer experience for the goal of writing sustainable modern web applications.</p>
+      <p>We believe in not sacrificing developer experience for the goal of writing sustainable, modern web applications. ✌️</p>
     `
   }]
 };
