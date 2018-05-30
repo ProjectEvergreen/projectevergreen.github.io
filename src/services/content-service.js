4215a3563ce2d2065ac04f7f89b184d3d18e3877
Owen Buckley <owen@thegreenhouse.io>
2018-05-30 19:37:44 -0400

    copy fixes

--- src/services/content-service.js
+++ src/services/content-service.js
@@ -14,7 +14,7 @@ const content = {
     template: html`
       <p>Component based development has become a widely adopted and pragmatic approach for modern web application development.</p>
       <br/>
-      <p>Reuse as a principal allows for native "Web" components that end up looking a lot like components you may already be used to seeing and writing.</p>
+      <p>Reuse as a principal encourages the use of native "Web" components that look a lot like the components you may already be used to seeing and writing.</p>
       <br/>
       <a href="https://jsfiddle.net/thegreenhouseio/qjj9hvv9/1/" target="_blank" rel="noopener">
         <img class="screenshot" src="https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/simple-component-example.jpg"/>
@@ -23,10 +23,10 @@ const content = {
   }, {
     title: 'Recycle',
     template: html`<p>It should be clear, the JavaScript community and ecosystem is amazing and inspiring! There are so many wonderful tools and options out there, and that's what
-      makes it so great. You pick and choose what you need when you need it.  In fact, this project wouldn't be possible without the amazing work of tools like
-      <b>Babel</b>, <b>postCSS</b>, and <b>webpack</b>. ❤️</p>
+      makes it so great. You pick and choose what you need when you need it.  In fact, this project wouldn't be possible without the amazing work of projects like
+      <b>Polymer</b>, <b>Babel</b>, <b>postCSS</b>, and <b>webpack</b>. ❤️</p>
       <br/>
-      <p>We believe in not sacrificing developer experience for the goal of writing sustainable, modern web applications. ✌️</p>
+      <p>We don't believe in sacrificing developer experience at the expense of writing sustainable, modern web applications. ✌️</p>
     `
   }]
 };
