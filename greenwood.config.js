1c924a2db684c4da25f855e2945431b87d052a8f
Owen Buckley <owenbuckley13@gmail.com>
2019-11-10 11:25:33 -0500

    metadata and google analytics

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,25 +1,31 @@
-// const pluginGoogleAnalytics = require('./packages/plugin-google-analytics/src/index');
+const pluginGoogleAnalytics = require('@greenwood/plugin-google-analytics');
 
-const META_DESCRIPTION = 'Website for Project Evergreen.';
 const FAVICON_HREF = '/assets/favicon.ico';
+const HANDLE = '@PrjEvergreen';
+const META_DESCRIPTION = `The goal of Project Evergreen is to provide detailed, accessible, and practical resources 
+to those looking to build modern, dynamic, performant, and evergreen web applications.`;
 
 module.exports = {
   title: 'Project Evergreen',
+
   meta: [
     { name: 'description', content: META_DESCRIPTION },
-    { name: 'twitter:site', content: '@PrjEvergreen' },
+    { name: 'twitter:site', content: HANDLE },
+    { name: 'twitter:creator', content: HANDLE },
     { property: 'og:title', content: 'Project Evergreen' },
     { property: 'og:type', content: 'website' },
     { property: 'og:url', content: 'https://projectevergreen.github.io' },
-    // { property: 'og:image', content: 'https://s3.amazonaws.com/hosted.greenwoodjs.io/greenwood-logo.png' },
+    { property: 'og:image', content: 'https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/logo.png' },
     { property: 'og:description', content: META_DESCRIPTION },
     { rel: 'shortcut icon', href: FAVICON_HREF },
-    { rel: 'icon', href: FAVICON_HREF }
-    // { name: 'google-site-verification', content: '4rYd8k5aFD0jDnN0CCFgUXNe4eakLP4NnA18mNnK5P0' }
+    { rel: 'icon', href: FAVICON_HREF },
+    { name: 'google-site-verification', content: 'rntZgfyG-5qkalTU3qnTEwZnhfJXIhFofM-wiM2Ym6I' }
   ],
+
   plugins: [
-    // ...pluginGoogleAnalytics({
-    //   analyticsId: 'UA-147204327-1'
-    // }),
+    ...pluginGoogleAnalytics({
+      analyticsId: 'UA-117350131-2'
+    })
   ]
+
 };
