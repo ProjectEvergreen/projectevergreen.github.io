5d29af6251512d8554e40552b9a95a766918c32a
Owen Buckley <owenbuckley13@gmail.com>
2021-04-10 13:18:18 -0400

    lazy load images

--- src/services/content-service.js
+++ src/services/content-service.js
@@ -15,7 +15,7 @@ const content = {
       <p>Reuse as a principal encourages the use of native "Web" components that look a lot like the components you may already be used to seeing and writing.</p>
       <br/>
       <a href="https://jsfiddle.net/thegreenhouseio/qjj9hvv9/1/" target="_blank" rel="noopener">
-        <img class="screenshot" src="https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/simple-component-example.jpg"/>
+        <img class="screenshot" src="https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/simple-component-example.jpg" loading=lazy/>
       </a>
     `
   }, {
