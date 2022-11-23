755de51e9719780d9e78b8c4b0c44bd3e4fb4980
thescientist13 <thescientist13@users.noreply.github.com>
2022-11-23 21:04:36 +0000

    deploy: 287ad2680381e706e2dd8ace08ed19a8c22ff1a2

--- footer.eb278697.js
+++ footer.eb278697.js
@@ -1,10 +1,10 @@
 import{L as e,h as t}from"./lit-element.4e4bdcd5.js";customElements.define("x-footer",class extends e{render(){return t`
       <style>
-        ${":host{background-color:#192a27;display:flex;justify-content:center;padding:2rem 0;text-align:center}:host a{color:#fff;text-decoration:none}"}
+        ${":host {   display: flex;   justify-content: center;   text-align: center;   padding: 2rem 0;   background-color: #192a27; }    :host a {     color: #fff;     text-decoration: none;   } "}
       </style>
 
       <footer>
         <a href="https://projectevergreen.github.io/">&copy; Project Evergreen ${(new Date).getFullYear()}</a>
       </footer>
     `}});
-//# sourceMappingURL=footer.33539478.js.map
+//# sourceMappingURL=footer.eb278697.js.map
